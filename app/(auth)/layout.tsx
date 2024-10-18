3d56a96e078c372bc52b6ab082c31c484e45c0b2
gourab8389 <deyg6988@gmail.com>
2024-10-18 20:18:17 +0530

    add AuthLayout and SignUpPage components; include logo SVG

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+
+interface AuthLayoutProps {
+    children: React.ReactNode;
+}
+
+const AuthLayout = ({children}: AuthLayoutProps) => {
+    return (
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                {children}
+            </div>
+        </main>
+    );
+}
+
+export default AuthLayout;
