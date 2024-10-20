6e4d47c67290698d3b81bc0721975cae5e51b9f4
gourab8389 <deyg6988@gmail.com>
2024-10-20 21:09:26 +0530

    refactor authentication hooks: rename useLogin to useRegister and create new useLogin hook

--- features/auth/api/use-register.ts
+++ features/auth/api/use-register.ts
@@ -3,18 +3,18 @@ import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
 
-type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>;
+type ResponseType = InferResponseType<typeof client.api.auth.login["$post"]>;
 
-type RequestType = InferRequestType<typeof client.api.auth.register["$post"]>;
+type RequestType = InferRequestType<typeof client.api.auth.login["$post"]>;
 
-export const useRegister = () => {
+export const useLogin = () => {
     const mutation = useMutation<
     ResponseType,
     Error,
     RequestType
     >({
        mutationFn: async ({json}) => {
-        const response = await client.api.auth.register["$post"]({ json });
+        const response = await client.api.auth.login["$post"]({ json });
         return await response.json();
        },
     });
