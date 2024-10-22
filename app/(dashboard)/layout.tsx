95c95432a8f23efa5f749dfa2b40d68f67827ecf
gourab8389 <deyg6988@gmail.com>
2024-10-22 20:02:23 +0530

    feat(dashboard): add Navbar component and integrate it into DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/dashboard/navbar";
 import { Sidebar } from "@/components/dashboard/sidebar";
 
 interface DashboardLayoutProps {
@@ -13,8 +14,9 @@ const DashboardLayout = (
                 <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
                     <Sidebar/>
                 </div>
-                <div className="lg:pl-[264px]">
+                <div className="lg:pl-[264px] w-full">
                     <div className="mx-auto max-w-screen-2xl h-full">
+                        <Navbar/>
                         <main className="h-full py-8 px-6 flex flex-col">
                             {children}
                         </main>
