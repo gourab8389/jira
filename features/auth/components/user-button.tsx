92a5077c52bd986447e056cc468815ab4b8536f1
gourab8389 <deyg6988@gmail.com>
2024-10-22 15:32:41 +0530

    feat(auth): enhance UserButton component with logout functionality and dropdown menu

--- features/auth/components/user-button.tsx
+++ features/auth/components/user-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Loader } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Loader, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -14,33 +14,61 @@ import { useLogout } from "../api/use-logout";
 import { useCurrent } from "../api/use-current";
 
 export const UserButton = () => {
-    const {data: user, isLoading} = useCurrent();
+    const { data: user, isLoading } = useCurrent();
+    const { mutate: logout } = useLogout();
 
-    if(isLoading){
+    if (isLoading) {
         return (
             <div className="size-10 rounded-full flex items-center justify-center bg-neutral-200 border-neutral-300">
-                <Loader 
-                className="size-4 animate-spin text-muted-foreground"
+                <Loader
+                    className="size-4 animate-spin text-muted-foreground"
                 />
             </div>
         )
     }
 
-    if(!user){
+    if (!user) {
         return null;
     }
 
     const { name, email } = user;
 
-    const avatarFallback = name 
-    ? name.charAt(0).toUpperCase()
-    : email.charAt(0).toUpperCase() ?? "U";
+    const avatarFallback = name
+        ? name.charAt(0).toUpperCase()
+        : email.charAt(0).toUpperCase() ?? "U";
 
     return (
-        <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
-            <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 items-center justify-center">
-                {avatarFallback}
-            </AvatarFallback>
-        </Avatar>
+        <DropdownMenu modal={false}>
+            <DropdownMenuTrigger>
+                <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
+                    <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 items-center justify-center">
+                        {avatarFallback}
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" side="bottom" className="w-60" sideOffset={10}>
+                <div className="flex flex-col items-center justify-self-center gap-2 px-2.5 py-4">
+                    <Avatar className="size-[52px] border border-neutral-300">
+                        <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 items-center justify-center text-xl">
+                            {avatarFallback}
+                        </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col items-center justify-center">
+                        <p className="text-sm font-medium text-neutral-900">
+                            {name || "User"}
+                        </p>
+                        <p className="text-sm font-medium text-neutral-900">{email}</p>
+                    </div>
+                </div>
+                <DottedSeparator className="mb-1"/>
+                <DropdownMenuItem
+                onClick={() => logout()}
+                className="h-10 flex items-center justify-center text-amber-700 font-medium cursor-pointer"
+                >
+                    <LogOut className="size-4 mr-2"/>
+                    Log out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
