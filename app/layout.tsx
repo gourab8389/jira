d997dd02519157885550518ff3bc56ca49f6f94d
gourab8389 <deyg6988@gmail.com>
2024-10-22 17:05:15 +0530

    feat(dashboard): add DashboardLayout component for structured layout and styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google"
 
 import { cn } from "@/lib/utils";
-import { QueryProvider } from "@/components/query-provider";
+import { QueryProvider } from "@/components/shared/query-provider";
 
 import "./globals.css";
 
