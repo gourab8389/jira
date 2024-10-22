27bfa9ce482fdde59244b93b251648eaa4be0145
gourab8389 <deyg6988@gmail.com>
2024-10-22 15:41:15 +0530

    feat(auth): refactor Home component and add server-side getCurrent function for user session management

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-"use client"
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useCurrent } from "@/features/auth/api/use-current";
 import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
-const router = useRouter();
-const { data, isLoading } = useCurrent();
-const { mutate } = useLogout();
 
-  useEffect(() => {
-    if (!isLoading && !data) {
-      router.push("/sign-in");
-    }
-  }, [data]);
 
   return (
     <div>
