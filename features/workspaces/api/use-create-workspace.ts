edea0b6af961f056f1607c4ecfbdd3f502144156
gourab8389 <deyg6988@gmail.com>
2024-10-24 16:35:25 +0530

    feat(workspaces): implement custom hook for creating workspaces with mutation handling

---
+++ features/workspaces/api/use-create-workspace.ts
@@ -0,0 +1,30 @@
+import { useRouter } from "next/navigation";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<typeof client.api.auth.login["$post"]>;
+
+type RequestType = InferRequestType<typeof client.api.auth.login["$post"]>;
+
+export const useCreateWorkspace = () => {
+    const router = useRouter();
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<
+    ResponseType,
+    Error,
+    RequestType
+    >({
+       mutationFn: async ({json}) => {
+        const response = await client.api.auth.login["$post"]({ json });
+        return await response.json();
+       },
+       onSuccess: () => {
+        router.refresh();
+        queryClient.invalidateQueries({ queryKey: ["current"] });
+       }
+    });
+    return mutation;
+};
