3c658d4a9dcd7c6e5446b0ad0bdedbbba6a95bc0
gourab8389 <deyg6988@gmail.com>
2024-10-24 23:57:05 +0530

    feat(workspaces): update dashboard styles and enhance WorkspaceSwitcher layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   
 
   return (
-    <div className="bg-neutral-500 p-4 h-full rounded-md">
+    <div className="bg-neutral-200 p-4 h-full rounded-md">
       <CreateWorkspaceForm/>
     </div>
   )
--- components/dashboard/workspaceSwitcher.tsx
+++ components/dashboard/workspaceSwitcher.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { RiAddCircleFill } from "react-icons/ri";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 
 export const WorkspaceSwitcher = () => {
     const { data } = useGetWorkspaces();
     return (
-        <div className="">
-            {JSON.stringify(data)}
+        <div className="flex flex-col gap-y-2">
+            <div className="flex items-center justify-between">
+                <p className="text-xs uppercase text-neutral-500">Workspaces</p>
+                <RiAddCircleFill className="text-neutral-500 size-5 cursor-pointer hover:opacity-75 transition"/>
+            </div>
         </div>
     )
 }
