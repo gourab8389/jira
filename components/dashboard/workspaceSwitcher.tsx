ff10800f9e37f614ecd6fde9e11568ec298dd59e
gourab8389 <deyg6988@gmail.com>
2024-10-25 00:00:06 +0530

    feat(workspaces): add Select component to WorkspaceSwitcher for workspace selection

--- components/dashboard/workspaceSwitcher.tsx
+++ components/dashboard/workspaceSwitcher.tsx
@@ -3,6 +3,14 @@
 import { RiAddCircleFill } from "react-icons/ri";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
+} from "@/components/ui/select";
+
 export const WorkspaceSwitcher = () => {
     const { data } = useGetWorkspaces();
     return (
@@ -11,6 +19,11 @@ export const WorkspaceSwitcher = () => {
                 <p className="text-xs uppercase text-neutral-500">Workspaces</p>
                 <RiAddCircleFill className="text-neutral-500 size-5 cursor-pointer hover:opacity-75 transition"/>
             </div>
+            <Select>
+                <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
+                    <SelectValue placeholder="No workspaces slected"/>
+                </SelectTrigger>
+            </Select>
         </div>
     )
 }
