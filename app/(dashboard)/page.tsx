37ca0c337199683d0cb5f35b102980bcdb922084
gourab8389 <deyg6988@gmail.com>
2024-10-25 23:17:46 +0530

    feat(dashboard): add loading component and clean up Home page layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -14,10 +14,4 @@ export default async function Home() {
     redirect(`/workspaces/${workspaces.documents[0].$id}`);
   }
   
-
-  return (
-    <div className="">
-      Home Page
-    </div>
-  )
 }
