7029018ade3f2549b251d2b3c123bf065179fd3c
gourab8389 <deyg6988@gmail.com>
2024-10-26 03:03:53 +0530

    feat(workspaces): add user authentication check to workspace pages and redirect to sign-in if not authenticated

--- app/(standalone)/workspaces/create/page.tsx
+++ app/(standalone)/workspaces/create/page.tsx
@@ -1,7 +1,16 @@
+import { redirect } from 'next/navigation';
+
+import { getCurrent } from '@/features/auth/actions';
 import { CreateWorkspaceForm } from '@/features/workspaces/components/create-workspaces-form';
 
 
-const WorkspaceCreatePage = () => {
+const WorkspaceCreatePage = async () => {
+    
+    const user = await getCurrent();
+
+    if(!user) redirect("/sign-in");
+
+
   return (
     <div className='w-full lg:max-w-xl'>
       <CreateWorkspaceForm/>
