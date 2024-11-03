f63ea85c2a28eaacfe6c76d9b6753838a8b14c25
gourab8389 <deyg6988@gmail.com>
2024-11-04 00:49:25 +0530

    feat(members): update MembersList component to display members and add navigation back to workspace

--- features/workspaces/components/members-list.tsx
+++ features/workspaces/components/members-list.tsx
@@ -1,22 +1,47 @@
 "use client";
 
+import { Fragment } from "react";
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
+
+import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { DottedSeparator } from "@/components/shared/dotted-separator";
+
+
 
 export const MembersList = () => {
-    const workspaceId = useWorkspaceId();
-    return (
-        <Card className="w-full h-full border-none shadow-none">
-            <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
-                <Button>
-                    
-                </Button>
-            </CardHeader>
-            <CardContent>
-                <div>Workspace ID: {workspaceId}</div>
-            </CardContent>
-        </Card>
-    )
-}
+  const workspaceId = useWorkspaceId();
+    const { data } = useGetMembers({ workspaceId });
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
+        <Button asChild variant={"secondary"} size={"sm"}>
+          <Link href={`/workspaces/${workspaceId}`}>
+            <ArrowLeftIcon className="size-4 mr-2" />
+            Back
+          </Link>
+        </Button>
+        <CardTitle className="text-xl font-bold">
+            Members List
+        </CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator/>
+      </div>
+      <CardContent className="p-7">
+        {data?.documents.map((member, index) => (
+            <Fragment key={member.$id}>
+                <div className="flex items-center gap-2">
+                
+                </div>
+            </Fragment>
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
