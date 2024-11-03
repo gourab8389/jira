2aa799d2f62dc828cff9977236878d10405504df
gourab8389 <deyg6988@gmail.com>
2024-11-03 23:35:27 +0530

    feat(members): implement member retrieval with authorization check and add users client to admin

--- features/members/server/route.ts
+++ features/members/server/route.ts
@@ -1,6 +1,37 @@
+import { z } from "zod";
 import { Hono } from "hono";
 
-const app = new Hono();
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { zValidator } from "@hono/zod-validator";
+import { createAdminClient } from "@/lib/appwrite";
+import { getMember } from "../utils";
+
+const app = new Hono()
+.get(
+    "/",
+    sessionMiddleware,
+    zValidator("query", z.object({
+        workspaceId: z.string()
+    })),
+    async (c) => {
+        const { users } = await createAdminClient();
+        const databases = c.get("databases");
+        const user = c.get("user");
+        const { workspaceId } = c.req.valid("query");
+
+        const member = await getMember({
+            databases,
+            workspaceId,
+            userId: user.$id
+        });
+
+        if(!member){
+            return c.json({
+                error: "Unauthorized"
+            }, 404);
+        }
+    }
+)
 
 export default app;
 
