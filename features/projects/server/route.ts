a3addd77cadffa3a35e619871989d507ef3c4d04
gourab8389 <deyg6988@gmail.com>
2024-11-04 20:50:16 +0530

    feat(projects): add ProjectAvatar component for displaying project images and initials

--- features/projects/server/route.ts
+++ features/projects/server/route.ts
@@ -57,7 +57,6 @@ const app = new Hono()
             ID.unique(),
             {
                 name,
-                userId: user.$id,
                 imageUrl: uploadedImageUrl,
                 workspaceId
             },
