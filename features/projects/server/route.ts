8f45096c9cad5be6a21fc379085b02dec37a03eb
gourab8389 <deyg6988@gmail.com>
2024-11-04 19:45:35 +0530

    feat(projects): implement project creation endpoint with image upload and authorization

--- features/projects/server/route.ts
+++ features/projects/server/route.ts
@@ -1,14 +1,72 @@
 import { z } from "zod";
 import { Hono } from "hono";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
 
 import { getMember } from "@/features/members/utils";
 
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { createProjectSchema } from "../schema";
 
 const app = new Hono()
+.post(
+    "/",
+    sessionMiddleware,
+    zValidator("form", createProjectSchema),
+    async (c) => {
+        const databases = c.get("databases");
+        const storage = c.get("storage")
+        const user = c.get("user");
+
+        const { name, image, workspaceId } = c.req.valid("form");
+
+        const member = await getMember({
+            databases,
+            workspaceId,
+            userId: user.$id,
+        })
+
+        if(!member){
+            return c.json({ error: "unauthorized" }, 401)
+        }
+
+        let uploadedImageUrl: string | undefined;
+
+        if(image instanceof File){
+            const file = await storage.createFile(
+                IMAGES_BUCKET_ID,
+                ID.unique(),
+                image,
+            );
+
+            const arrayBuffer = await storage.getFilePreview(
+                IMAGES_BUCKET_ID,
+                file.$id,
+            );
+
+            uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+        }
+
+
+        
+
+        const project = await databases.createDocument(
+            DATABASE_ID,
+            PROJECTS_ID,
+            ID.unique(),
+            {
+                name,
+                userId: user.$id,
+                imageUrl: uploadedImageUrl,
+                workspaceId
+            },
+        );
+
+
+        return c.json({ data: project });
+    }
+)
 .get(
     "/",
     sessionMiddleware,
