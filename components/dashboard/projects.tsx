18320ac0ec99c7e679b143dbe3e50b3fb32de153
gourab8389 <deyg6988@gmail.com>
2024-11-04 21:08:33 +0530

    feat(projects): integrate ProjectAvatar component into Projects dashboard and adjust avatar sizing

--- components/dashboard/projects.tsx
+++ components/dashboard/projects.tsx
@@ -7,8 +7,9 @@ import { RiAddCircleFill } from "react-icons/ri";
 import { cn } from "@/lib/utils";
 
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
-import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 
 
@@ -43,6 +44,7 @@ const Projects = () => {
                             "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500",
                             isActive && "bg-white shadow-sm hover:opacity-100 text-primary",
                             )}>
+                                <ProjectAvatar image={project.imageUrl} name={project.name}/>
                             <span className="truncate">
                                 {project.name}
                             </span>
--- features/projects/components/project-avatar.tsx
+++ features/projects/components/project-avatar.tsx
@@ -11,17 +11,19 @@ interface ProjectAvatarProps {
     image?: string;
     name: string;
     className?: string;
+    fallbackClassName?: string;
 }
 
 export const ProjectAvatar = ({
     image,
     name,
-    className
+    className,
+    fallbackClassName,
 }: ProjectAvatarProps) => {
     if (image) {
         return (
             <div 
-            className={cn("size-10 rounded-md relative overflow-hidden", className)}>
+            className={cn("size-5 rounded-md relative overflow-hidden", className)}>
                 <Image
                     src={image}
                     alt={name}
@@ -34,10 +36,13 @@ export const ProjectAvatar = ({
 
     return (
         <Avatar
-            className={cn("size-10 rounded-md", className)}
+            className={cn("size-5 rounded-md", className)}
         >
             <AvatarFallback
-                className="text-white bg-blue-600 font-semibold text-lg uppercase rounded-md"
+                className={cn(
+                    "text-white bg-blue-600 font-semibold text-sm uppercase rounded-md",
+                    fallbackClassName,
+                )}
             >
                 {name[0]}
             </AvatarFallback>
