94ae22b2439b85894720f1d9e89a9364bd0a86fc
gourab8389 <deyg6988@gmail.com>
2024-11-04 23:55:13 +0530

    feat(projects): update project endpoint to use projectId and improve project retrieval

--- features/projects/server/route.ts
+++ features/projects/server/route.ts
@@ -7,7 +7,8 @@ import { getMember } from "@/features/members/utils";
 
 import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { createProjectSchema } from "../schemas";
+import { createProjectSchema, updateProjectSchema } from "../schemas";
+import { Project } from "../types";
 
 const app = new Hono()
 .post(
@@ -103,24 +104,30 @@ const app = new Hono()
     }
 )
 .patch(
-    "/:workspaceId",
+    "/:projectId",
     sessionMiddleware,
-    zValidator("form", updateWorkspaceSchema),
+    zValidator("form", updateProjectSchema),
     async (c) => {
         const databases = c.get("databases");
         const storage = c.get("storage")
         const user = c.get("user"); 
 
-        const { workspaceId } = c.req.param();
+        const { projectId } = c.req.param();
         const { name, image } = c.req.valid("form");
+
+        const existingProject = await databases.getDocument<Project>(
+            DATABASE_ID,
+            PROJECTS_ID,
+            projectId,
+        );
 
         const member = await getMember({
             databases,
-            workspaceId,
+            workspaceId: existingProject.workspaceId,
             userId: user.$id,
         });
 
-        if(!member || member.role !== MemberRole.ADMIN){
+        if(!member){
             return c.json({
                 error: "unauthorized"
             }, 401);
@@ -145,17 +152,17 @@ const app = new Hono()
             uploadedImageUrl = image;
         }
 
-        const workspace = await databases.updateDocument(
+        const project = await databases.updateDocument(
             DATABASE_ID,
-            WORKSPACES_ID,
-            workspaceId,
+            PROJECTS_ID,
+            projectId,
             {
                 name,
                 imageUrl: uploadedImageUrl
             }
         );
 
-        return c.json({ data: workspace });
+        return c.json({ data: project });
     }
 )
 
