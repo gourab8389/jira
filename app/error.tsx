0b41cd13bb89bc4d1701de3a67fab614d4e35b87
gourab8389 <deyg6988@gmail.com>
2024-11-04 23:28:29 +0530

    feat(loading): create LoadingPage component and update loading styles

--- app/error.tsx
+++ app/error.tsx
@@ -8,8 +8,8 @@ import { Button } from "@/components/ui/button";
 const ErrorPage = () => {
   return (
     <div className="h-screen flex flex-col gap-y-4 items-center justify-center">
-      <AlertTriangle className="size-6"/>
-      <p className="text-sm">
+      <AlertTriangle className="size-6 text-muted-foreground"/>
+      <p className="text-sm text-muted-foreground">
         Something went wrong. Please try again later.
       </p>
       <Button variant={"secondary"} size="sm">
