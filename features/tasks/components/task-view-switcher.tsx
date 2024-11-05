1c82b086ef1bfbc8d12b97f4f98b1383df6359c8
gourab8389 <deyg6988@gmail.com>
2024-11-05 23:26:48 +0530

    feat(tasks): enhance TaskViewSwitcher with new button and data filters

--- features/tasks/components/task-view-switcher.tsx
+++ features/tasks/components/task-view-switcher.tsx
@@ -1,3 +1,7 @@
+import { PlusIcon } from "lucide-react"
+
+import { DottedSeparator } from "@/components/shared/dotted-separator"
+import { Button } from "@/components/ui/button"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export const TaskViewSwitcher = () => {
@@ -25,7 +29,27 @@ export const TaskViewSwitcher = () => {
                             Calendar
                         </TabsTrigger>
                     </TabsList>
+                    <Button size={"sm"} className="w-full lg:w-auto">
+                        <PlusIcon className="size-4 mr-2"/>
+                        New 
+                    </Button>
                 </div>
+                <DottedSeparator className="my-4"/>
+                Data filters
+                <DottedSeparator className="my-4"/>
+                <>
+                <TabsContent value="table" className="mt-0">
+                    Data Table
+                </TabsContent>
+
+                <TabsContent value="kanban" className="mt-0">
+                    Data Kanban
+                </TabsContent>
+
+                <TabsContent value="calendar" className="mt-0">
+                    Data Calendar
+                </TabsContent>
+                </>
             </div>
         </Tabs>
     )
