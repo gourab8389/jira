91ddd6244d8ebb6c25f65dc6d761cb978d45b5d5
gourab8389 <deyg6988@gmail.com>
2024-11-07 18:11:40 +0530

    feat(tasks): add sortable header for Task Name in columns definition

--- features/tasks/components/columns.tsx
+++ features/tasks/components/columns.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { ArrowUpDown } from "lucide-react"
 import { ColumnDef } from "@tanstack/react-table"
+
+import { Button } from "@/components/ui/button";
+
 import { Task } from "../types";
 
 export const columns: ColumnDef<Task>[] = [
     {
         accessorKey: "name",
-        header: "Task Name"
+        header: ({ column }) => {
+            return (
+              <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                Task Name
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+              </Button>
+            )
+          },
     },
 ];
