43256c01bbf74e559f2fbef8590264fa1ca863d2
gourab8389 <deyg6988@gmail.com>
2024-11-07 18:53:48 +0530

    feat(tasks): add project column with avatar and sorting functionality to DataTable

--- features/tasks/components/columns.tsx
+++ features/tasks/components/columns.tsx
@@ -3,6 +3,8 @@
 import { ArrowUpDown } from "lucide-react"
 import { ColumnDef } from "@tanstack/react-table"
 
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+
 import { Button } from "@/components/ui/button";
 
 import { Task } from "../types";
@@ -21,5 +23,40 @@ export const columns: ColumnDef<Task>[] = [
               </Button>
             )
           },
+          cell: ({ row }) => {
+            const name = row.original.name;
+            return (
+                <p className="line-clamp-1">{name}</p>
+            )
+          },
     },
+    {
+        accessorKey: "project",
+        header: ({ column }) => {
+            return (
+                <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                Project
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+        cell: ({ row }) => {
+            const project = row.original.project;
+            return (
+                <div className="flex items-center gap-x-2 text-sm font-medium">
+                    <ProjectAvatar
+                    className="size-6"
+                    name={project.name}
+                    image={project.image} 
+                    />
+                    <p className="line-clamp-1">
+                        {project.name}
+                    </p>
+                </div>
+            )
+        }
+    }
 ];
