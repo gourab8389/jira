68d0eec0d63e2bfae6bc1766d0846d34891bec92
gourab8389 <deyg6988@gmail.com>
2024-11-07 15:33:11 +0530

    feat(tasks): integrate DataFilters component into TaskViewSwitcher for enhanced task filtering

--- features/tasks/components/data-filters.tsx
+++ features/tasks/components/data-filters.tsx
@@ -1,8 +1,11 @@
+import { ListChecksIcon } from "lucide-react";
+
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 import { DatePicker } from "@/components/shared/date-picker";
+import { DottedSeparator } from "@/components/shared/dotted-separator";
 import { 
     Select,
     SelectContent,
@@ -11,6 +14,9 @@ import {
     SelectSeparator,
     SelectValue,
  } from "@/components/ui/select";
+
+import { TaskStatus } from "../types";
+
 
 interface DataFiltersProps {
     hideProjectFilter?: boolean;
@@ -42,7 +48,26 @@ const DataFilters = ({
 
   return (
     <div className="flex flex-col lg:flex-row gap-2">
-      
+      <Select
+      defaultValue={undefined}
+      onValueChange={() => {}}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8">
+            <div className="flex items-center pr-2">
+                <ListChecksIcon className="size-4 h-4 w-4 mr-2" />
+                <SelectValue placeholder="All Statuses"/>
+            </div>
+        </SelectTrigger>
+        <SelectContent>
+            <SelectItem value="all">All Statuses</SelectItem>
+            <DottedSeparator/>
+            <SelectItem value={TaskStatus.BACKLOG}>Backlog</SelectItem>
+            <SelectItem value={TaskStatus.IN_PROGRESS}>In Progress</SelectItem>
+            <SelectItem value={TaskStatus.IN_REVIEW}>In Review</SelectItem>
+            <SelectItem value={TaskStatus.TODO}>Todo</SelectItem>
+            <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   )
 }
--- features/tasks/components/task-view-switcher.tsx
+++ features/tasks/components/task-view-switcher.tsx
@@ -1,92 +1,79 @@
 "use client";
 
-import { Loader, PlusIcon } from "lucide-react"
+import { Loader, PlusIcon } from "lucide-react";
 import { useQueryState } from "nuqs";
 
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
-import { DottedSeparator } from "@/components/shared/dotted-separator"
-import { Button } from "@/components/ui/button"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { DottedSeparator } from "@/components/shared/dotted-separator";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import { useGetTasks } from "../api/use-get-tasks"; 
-import { useCreateTaskModal } from "../hooks/use-create-task-modal"
+import DataFilters from "./data-filters";
 
+import { useGetTasks } from "../api/use-get-tasks";
+import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 
 export const TaskViewSwitcher = () => {
-    const [ view, setView ] = useQueryState("task-view", {
-        defaultValue: "table",
-    });
+  const [view, setView] = useQueryState("task-view", {
+    defaultValue: "table",
+  });
 
-    const workspaceId = useWorkspaceId();
+  const workspaceId = useWorkspaceId();
 
-    const { 
-        data: tasks, 
-        isLoading: isLoadingTasks 
-    } = useGetTasks({ workspaceId });
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+  });
 
-    const { open } = useCreateTaskModal();
+  const { open } = useCreateTaskModal();
 
-    return (
-        <Tabs
-        defaultValue={view}
-        onValueChange={setView} 
-        className="flex-1 w-full border rounded-lg"
-        >
-            <div className="h-full flex flex-col overflow-auto p-4">
-                <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
-                    <TabsList className="w-full lg:w-auto">
-                        <TabsTrigger
-                        className="h-8 w-full lg:w-auto"
-                        value="table"
-                        >
-                            Table
-                        </TabsTrigger>
-                        <TabsTrigger
-                        className="h-8 w-full lg:w-auto"
-                        value="kanban"
-                        >
-                            Kanban
-                        </TabsTrigger>
-                        <TabsTrigger
-                        className="h-8 w-full lg:w-auto"
-                        value="calendar"
-                        >
-                            Calendar
-                        </TabsTrigger>
-                    </TabsList>
-                    <Button 
-                    size={"sm"} 
-                    className="w-full lg:w-auto"
-                    onClick={open}
-                    >
-                        <PlusIcon className="size-4 mr-2"/>
-                        New 
-                    </Button>
-                </div>
-                <DottedSeparator className="my-4"/>
-                Data filters
-                <DottedSeparator className="my-4"/>
-                {isLoadingTasks ? (
-                    <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
-                       <Loader  className="size-5 animate-spin text-muted-foreground" />
-                    </div>
-                ) : (
-                <>
-                <TabsContent value="table" className="mt-0">
-                    {JSON.stringify(tasks)}
-                </TabsContent>
+  return (
+    <Tabs
+      defaultValue={view}
+      onValueChange={setView}
+      className="flex-1 w-full border rounded-lg"
+    >
+      <div className="h-full flex flex-col overflow-auto p-4">
+        <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
+              Table
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
+              Kanban
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
+              Calendar
+            </TabsTrigger>
+          </TabsList>
+          <Button size={"sm"} className="w-full lg:w-auto" onClick={open}>
+            <PlusIcon className="size-4 mr-2" />
+            New
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <DataFilters />
+        <DottedSeparator className="my-4" />
+        {isLoadingTasks ? (
+          <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="table" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
 
-                <TabsContent value="kanban" className="mt-0">
-                {JSON.stringify(tasks)}
-                </TabsContent>
+            <TabsContent value="kanban" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
 
-                <TabsContent value="calendar" className="mt-0">
-                {JSON.stringify(tasks)}
-                </TabsContent>
-                </>
-                )}
-            </div>
-        </Tabs>
-    )
-}
+            <TabsContent value="calendar" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+          </>
+        )}
+      </div>
+    </Tabs>
+  );
+};
