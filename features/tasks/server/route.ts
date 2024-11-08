89a2b581a7ebdabfcb2a197cd646dbe635a78987
gourab8389 <deyg6988@gmail.com>
2024-11-09 03:50:16 +0530

    feat(tasks): add GET endpoint for retrieving task details and refactor existing routes for improved readability

--- features/tasks/server/route.ts
+++ features/tasks/server/route.ts
@@ -14,7 +14,10 @@ import { createTaskSchema } from "../schemas";
 import { Task, TaskStatus } from "../types";
 
 const app = new Hono()
-  .delete(":taskId", sessionMiddleware, async (c) => {
+  .delete(
+    "/:taskId", 
+    sessionMiddleware, 
+    async (c) => {
     const user = c.get("user");
     const databases = c.get("databases");
     const { taskId } = c.req.param();
@@ -58,8 +61,14 @@ const app = new Hono()
       const databases = c.get("databases");
       const user = c.get("user");
 
-      const { workspaceId, projectId, status, search, assigneeId, dueDate } =
-        c.req.valid("query");
+      const { 
+        workspaceId, 
+        projectId, 
+        status, 
+        search, 
+        assigneeId, 
+        dueDate 
+      } = c.req.valid("query");
 
       const member = await getMember({
         databases,
@@ -163,8 +172,14 @@ const app = new Hono()
     async (c) => {
       const user = c.get("user");
       const databases = c.get("databases");
-      const { name, status, workspaceId, projectId, dueDate, assigneeId } =
-        c.req.valid("json");
+      const { 
+        name, 
+        status, 
+        workspaceId, 
+        projectId, 
+        dueDate, 
+        assigneeId 
+      } = c.req.valid("json");
 
       const member = await getMember({
         databases,
@@ -188,7 +203,7 @@ const app = new Hono()
       );
 
       const newPostion =
-        highestPositionTask?.documents.length > 0
+        highestPositionTask.documents.length > 0
           ? highestPositionTask.documents[0].position + 1000
           : 1000;
 
@@ -211,7 +226,7 @@ const app = new Hono()
     }
   )
   .patch(
-    ":taskId",
+    "/:taskId",
     sessionMiddleware,
     zValidator("json", createTaskSchema.partial()),
     async (c) => {
@@ -244,7 +259,7 @@ const app = new Hono()
         return c.json({ error: "Unauthorized" }, 401);
       }
 
-      const task = await databases.updateDocument(
+      const task = await databases.updateDocument<Task>(
         DATABASE_ID,
         TASKS_ID,
         taskId,
@@ -260,6 +275,61 @@ const app = new Hono()
 
       return c.json({ data: task });
     }
-  );
+  )
+  .get(
+    "/:taskId",
+    sessionMiddleware,
+    async (c) => {
+      const currentUser = c.get("user");
+      const databases = c.get("databases");
+      const { users } = await createAdminClient();
+      const { taskId } = c.req.param();
+
+      const task = await databases.getDocument<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        taskId
+      );
+
+      const currentUserMember = await getMember({
+        databases,
+        workspaceId: task.workspaceId,
+        userId: currentUser.$id,
+      });
+
+      if (!currentUserMember) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const project = await databases.getDocument<Project>(
+        DATABASE_ID,
+        PROJECTS_ID,
+        task.projectId
+      );
+
+      const member = await databases.getDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        task.assigneeId
+      )
+      
+      const user = await users.get(member.userId);
+
+      const assignee ={
+        ...member,
+        name: user.name,
+        email: user.email,
+      };
+
+      return c.json({
+        data: {
+          ...task,
+          project,
+          assignee,
+        },
+      });
+
+    }
+  )
 
 export default app;
