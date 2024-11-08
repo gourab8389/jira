65c52f22085539bdaf75f0ad82ea13b64cd09f37
gourab8389 <deyg6988@gmail.com>
2024-11-09 02:26:25 +0530

    feat(tasks): add EditTaskFormWrapper component and update EditTaskModal to use it for task editing

--- features/tasks/components/edit-task-modal.tsx
+++ features/tasks/components/edit-task-modal.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { ResponsiveModal } from "@/components/dashboard/responsive-modal";
 
-import { CreateTaskFormWrapper } from "./create-task-form-wrapper";
+import { EditTaskFormWrapper } from "./edit-task-form-wrapper";
 
 import { useEditTaskModal } from "../hooks/use-edit-task-modal";
 
 
 const EditTaskModal = () => {
-    const { taskId, setTaskId, close } = useEditTaskModal();
+    const { taskId, close } = useEditTaskModal();
   return (
     <ResponsiveModal open={!!taskId} onOpenChange={close}>
        {taskId && (
-         <CreateTaskFormWrapper onCancel={close}/>
+         <EditTaskFormWrapper onCancel={close} id={taskId}/>
        )}
     </ResponsiveModal>
   )
