63f8af65d40611c7bea052375abcaf2536b24a57
gourab8389 <deyg6988@gmail.com>
2024-11-09 15:42:40 +0530

    feat(tasks): implement DataKanban component with task state management and drag-and-drop functionality

--- features/tasks/components/data-kanban.tsx
+++ features/tasks/components/data-kanban.tsx
@@ -1,20 +1,66 @@
-import { 
-    DragDropContext,
-    Droppable,
-    Draggable,
-    DropResult,
- } from "@hello-pangea/dnd";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "@hello-pangea/dnd";
 
-import { Task } from "../types";
+import { Task, TaskStatus } from "../types";
+
+const boards: TaskStatus[] = [
+  TaskStatus.BACKLOG,
+  TaskStatus.TODO,
+  TaskStatus.IN_PROGRESS,
+  TaskStatus.IN_REVIEW,
+  TaskStatus.DONE,
+];
+
+type TaskState = {
+  [key in TaskStatus]: Task[];
+};
 
 interface DataKanbanProps {
-    data: Task[];
+  data: Task[];
 }
 
 export const DataKanban = ({ data }: DataKanbanProps) => {
-    return (
-        <div>
-           Data Kanban
-        </div>
-    );
-}
+  const [tasks, setTasks] = useState<TaskState>(() => {
+    const initialTasks: TaskState = {
+      [TaskStatus.BACKLOG]: [],
+      [TaskStatus.TODO]: [],
+      [TaskStatus.IN_PROGRESS]: [],
+      [TaskStatus.IN_REVIEW]: [],
+      [TaskStatus.DONE]: [],
+    };
+
+    data.forEach((task) => {
+      initialTasks[task.status].push(task);
+    });
+
+    Object.keys(initialTasks).forEach((status) => {
+      initialTasks[status as TaskStatus].sort(
+        (a, b) => a.position - b.position
+      );
+    });
+
+    return initialTasks;
+  });
+
+  return (
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="flex overflow-x-auto">
+        {boards.map((board) => {
+          return (
+            <div
+              key={board}
+              className="flex-1 mx-2 bg-muted p-1.5 rounded-md min-w-[200px]"
+            >
+                <KanbanColumnHeader/>
+            </div>
+          );
+        })}
+      </div>
+    </DragDropContext>
+  );
+};
