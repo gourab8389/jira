37eb13ac8cf63ab931ecd23f3f504832859c8d5c
gourab8389 <deyg6988@gmail.com>
2024-11-09 20:52:55 +0530

    feat(tasks): enhance DataCalendar component with event handling and navigation

--- features/tasks/components/data-calendar.tsx
+++ features/tasks/components/data-calendar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { 
     format,
     getDay,
@@ -6,9 +7,25 @@ import {
     addMonths,
     subMonths,
 } from "date-fns";
+import { enUS } from "date-fns/locale";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 
 import { Task } from "../types"
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+const locales = {
+    "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
 
 interface DataCalendarProps {
     data: Task[];
@@ -17,11 +34,48 @@ interface DataCalendarProps {
 const DataCalendar = ({
     data,
 }: DataCalendarProps) => {
+
+    const [value, setValue] = useState(
+      data.length > 0 ? new Date(data[0].dueDate) : new Date()
+    );
+
+    const events = data.map((task) => ({
+        start: new Date(task.dueDate),
+        end: new Date(task.dueDate),
+        title: task.name,
+        project: task.project,
+        assignee: task.assignee,
+        status: task.status,
+        id: task.$id,
+    }));
+
+    const handleNavigate = (action: "PREV" | "NEXT" | "TODAY") => {
+        if (action === "PREV") {
+            setValue(subMonths(value, 1));
+        } else if (action === "NEXT") {
+            setValue(addMonths(value, 1));
+        } else if (action === "TODAY") {
+            setValue(new Date());
+        }
+    };
+
   return (
-    <div>
-      Data Calender
-    </div>
+    <Calendar
+    localizer={localizer}
+    date={value}
+    events={events}
+    views={["month"]}
+    defaultDate="month"
+    toolbar
+    showAllEvents
+    className="h-full"
+    max={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
+    formats={{
+        weekdayFormat: (date, culture, localizer) => localizer?.format(date, "EEEE", culture) ?? ""
+    }}
+
+    />
   )
 }
 
-export default DataCalendar
+export default DataCalendar;
