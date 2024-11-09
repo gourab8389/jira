2dea3bc90d28981836f83e046278a446ca12a507
gourab8389 <deyg6988@gmail.com>
2024-11-09 23:58:53 +0530

    feat(tasks): add TaskBreadCrumbs component and handle loading/error states in TaskIdClient

--- app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import { PageError } from "@/components/shared/page-error";
 import { PageLoader } from "@/components/shared/page-loader";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { TaskBreadCrumbs } from "@/features/tasks/components/task-breadcrumbs";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 
 export const TaskIdClient = () => {
     const taskId = useTaskId();
     const { data, isLoading } = useGetTask({ taskId });
+
     if(isLoading) {
         return <PageLoader/>
     }
+
+    if(!data) {
+        return <PageError message="Task not found"/>
+    }
+
     return (
-        <div>
-            page id
+        <div className="flex flex-col">
+            <TaskBreadCrumbs project={data.project} task={data} />
         </div>
     )
 }
