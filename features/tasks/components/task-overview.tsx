30ecd411fc595b854bfade64583e3c82d38972ab
gourab8389 <deyg6988@gmail.com>
2024-11-10 01:41:13 +0530

    feat(tasks): add OverviewProperty for assignee and due date in TaskOverview

--- features/tasks/components/task-overview.tsx
+++ features/tasks/components/task-overview.tsx
@@ -1,7 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Task } from "../types";
 import { PencilIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import { DottedSeparator } from "@/components/shared/dotted-separator";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
+
+import { OverviewProperty } from "./overview-property";
+
+import { Task } from "../types";
+import TaskDate from "./task-date";
 
 interface TaskOverviewProps {
   task: Task;
@@ -19,7 +25,19 @@ export const TaskOverview = ({ task }: TaskOverviewProps) => {
         </div>
         <DottedSeparator className="my-4"/>
         <div className="flex flex-col gap-y-4">
-            
+            <OverviewProperty label="Assignee">
+                <MemberAvatar
+                name={task.assignee.name}
+                className="size-6"
+                />
+                <p className="text-sm font-medium">
+                {task.assignee.name}
+                </p>
+            </OverviewProperty>
+
+            <OverviewProperty label="Due Date">
+                <TaskDate value={task.dueDate} className="text-sm font-medium"/>
+            </OverviewProperty>
         </div>
       </div>
     </div>
