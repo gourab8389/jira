11ce7642b55a620a380fa6a495e1bb0857c1d1b7
gourab8389 <deyg6988@gmail.com>
2024-11-10 01:36:02 +0530

    feat(tasks): add OverviewProperty component and enhance TaskOverview layout

--- features/tasks/components/task-overview.tsx
+++ features/tasks/components/task-overview.tsx
@@ -1,20 +1,27 @@
 import { Button } from "@/components/ui/button";
 import { Task } from "../types";
 import { PencilIcon } from "lucide-react";
+import { DottedSeparator } from "@/components/shared/dotted-separator";
 
 interface TaskOverviewProps {
-    task: Task;
+  task: Task;
 }
 
-export const TaskOverview = ({
-     task 
-}: TaskOverviewProps) => {
+export const TaskOverview = ({ task }: TaskOverviewProps) => {
+  return (
     <div className="flex flex-col gap-y-4 col-span-1">
-        <div className="bg-muted rounded-lg p-4">
-            <p className="text-lg font-semibold">Overview</p>
-            <Button>
-                <PencilIcon/>
-            </Button>
+      <div className="bg-muted rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold">Overview</p>
+          <Button size={"sm"} variant={"secondary"}>
+            <PencilIcon className="size-4 mr-2" />
+          </Button>
         </div>
+        <DottedSeparator className="my-4"/>
+        <div className="flex flex-col gap-y-4">
+            
+        </div>
+      </div>
     </div>
-}
+  );
+};
