364835c1851d4815ba0cc3a6e9cd3e3ae78ab13f
gourab8389 <deyg6988@gmail.com>
2024-11-09 23:43:59 +0530

    feat(tasks): implement useTaskId hook and integrate task loading in TaskIdClient component

--- app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { useTaskId } from "@/features/tasks/hooks/use-task-id";
+
 export const TaskIdClient = () => {
+    const taskId = useTaskId();
+    const { data, isLoading } = useGetTask({ taskId });
+    if(isLoading) {
+        return <PageLoader/>
+    }
     return (
         <div>
             page id
