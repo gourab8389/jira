82339d23355473ddbafc0eb61e3ee60ec6656782
gourab8389 <deyg6988@gmail.com>
2024-11-09 22:39:39 +0530

    feat(tasks): add custom toolbar to DataCalendar for improved navigation

--- features/tasks/components/data-calendar.tsx
+++ features/tasks/components/data-calendar.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { 
+    CalendarIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon,
+} from "lucide-react";
+import { 
     format,
     getDay,
     parse,
@@ -9,6 +14,8 @@ import {
 } from "date-fns";
 import { enUS } from "date-fns/locale";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+
+import { Button } from "@/components/ui/button";
 
 import EventCard from "./event-card";
 
@@ -33,6 +40,42 @@ const localizer = dateFnsLocalizer({
 interface DataCalendarProps {
     data: Task[];
 }
+
+interface CustomToolbarProps {
+    date: Date;
+    onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+}
+
+const CustomToolbar = ({date, onNavigate}: CustomToolbarProps)=>{
+    return (
+        <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+            <Button
+            onClick={() => onNavigate("PREV")}
+            variant={"secondary"}
+            size={"icon"}
+            >
+                <ChevronLeftIcon className="size-4" />
+            </Button>
+
+            <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+                <CalendarIcon className="size-4 mr-2"/>
+                <p className="text-xs">
+                    {format(date, "MMMM yyyy")}
+                </p>
+            </div>
+
+            <Button
+            onClick={() => onNavigate("NEXT")}
+            variant={"secondary"}
+            size={"icon"}
+            >
+                <ChevronRightIcon className="size-4" />
+            </Button>
+        </div>
+    )
+}
+
+
 
 const DataCalendar = ({
     data,
@@ -85,6 +128,9 @@ const DataCalendar = ({
             assignee={event.assignee}
             status={event.status}
             />
+        ),
+        toolbar: () => (
+            <CustomToolbar date={value} onNavigate={handleNavigate}/>
         )
     }}
     />
