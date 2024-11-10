1986f29818bcbd0d6d9b6a0f37a2698f71cc9196
gourab8389 <deyg6988@gmail.com>
2024-11-10 15:18:39 +0530

    feat(workspaces): enhance WorkspaceIdSettingsClient with loading and error handling for project retrieval

--- app/(standalone)/workspaces/[workspaceId]/settings/client.tsx
+++ app/(standalone)/workspaces/[workspaceId]/settings/client.tsx
@@ -1,8 +1,24 @@
 "use client";
 
+import { PageError } from "@/components/shared/page-error";
+import { PageLoader } from "@/components/shared/page-loader";
 import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 export const WorkspaceIdSettingsClient = async () => {
+
+    const workspaceId = useWorkspaceId();
+
+    const { data: initialValues, isLoading } = useGetProject({ workspaceId });
+
+    if (isLoading) {
+        return <PageLoader/>;
+    }
+
+    if(!initialValues) {
+        return <PageError message="Project not found"/>;
+    }
+    
     return (
         <div className="w-full lg:max-w-xl">
         <EditWorkspaceForm initialValues={initialValues}/>
--- app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,6 +1,7 @@
+import { redirect } from "next/navigation";
+
 import { getCurrent } from "@/features/auth/queries"
 
-import { redirect } from "next/navigation";
 import { WorkspaceIdSettingsClient } from "./client";
 
 
