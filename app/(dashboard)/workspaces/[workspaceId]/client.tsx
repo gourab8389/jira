dd292a3021a49a836308775a9e78cafab395b080
gourab8389 <deyg6988@gmail.com>
2024-11-10 21:17:35 +0530

    feat(workspaces): add MembersList component to display projects with creation option

--- app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -160,3 +160,52 @@ export const ProjectList = ({ data, total }: ProjectListProps) => {
     </div>
   );
 };
+
+
+
+interface MembersListProps {
+    data: Member[];
+    total: number;
+  }
+  
+  export const MembersList = ({ data, total }: MembersListProps) => {
+    const workspaceId = useWorkspaceId();
+    const { open: createProject } = useCreateProjectModal();
+    return (
+      <div className="flex flex-col gap-y-1 col-span-1">
+        <div className="bg-white border rounded-lg p-4">
+          <div className="flex items-center justify-between">
+            <p className="text-lg font-semibold">Projects ({total})</p>
+            <Button variant={"secondary"} size={"icon"} onClick={createProject}>
+              <PlusIcon className="size-4 text-neutral-400" />
+            </Button>
+          </div>
+          <DottedSeparator className="my-4" />
+          <ul className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            {data.map((project) => (
+              <li key={project.$id}>
+                <Link href={`workspaces/${workspaceId}/projects/${project.$id}`}>
+                  <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                    <CardContent className="p-4 flex items-center gap-x-2.5">
+                      <ProjectAvatar
+                      className="size-12"
+                      fallbackClassName="text-lg"
+                      name={project.name}
+                      image={project.imageUrl}
+                      />
+                      <p className="text-lg font-medium truncate">
+                          {project.name}
+                      </p>
+                    </CardContent>
+                  </Card>
+                </Link>
+              </li>
+            ))}
+            <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+              No tasks found
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
