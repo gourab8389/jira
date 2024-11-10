84f6eb7c9ba8ef09367e9a13615abdf3b0ea4894
gourab8389 <deyg6988@gmail.com>
2024-11-10 17:26:42 +0530

    feat(projects): enhance project details by integrating project analytics

--- app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -12,17 +12,22 @@ import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher
 import { Button } from "@/components/ui/button";
 import { PageLoader } from "@/components/shared/page-loader";
 import { PageError } from "@/components/shared/page-error";
+import { useGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics";
 
 
 export const ProjectIdClient = () => {
-    const projectId = useProjectId()
-    const { data, isLoading } = useGetProject({ projectId });
+    const projectId = useProjectId();
+
+    const { data: project, isLoading: isLoadingProject } = useGetProject({ projectId });
+    const { data: analytics, isLoading: iaLoadingAnalytics } = useGetProjectAnalytics({ projectId });
+
+    const isLoading = isLoadingProject || iaLoadingAnalytics;
 
     if(isLoading){
         return <PageLoader/>
     }
 
-    if(!data){
+    if(!project || !analytics){
         return <PageError message="Project not found"/>;
     }
     return (
@@ -30,15 +35,15 @@ export const ProjectIdClient = () => {
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-x-2">
             <ProjectAvatar
-              name={data.name}
-              image={data.imageUrl}
+              name={project.name}
+              image={project.imageUrl}
               className="size-8"
             />
-            <p className="text-lg font-semibold">{data.name}</p>
+            <p className="text-lg font-semibold">{project.name}</p>
           </div>
           <div className="">
               <Button variant={"secondary"} size={"sm"} asChild>
-                  <Link href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}>
+                  <Link href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}>
                       <PencilIcon className="size-4 mr-2"/>
                       Edit Project
                   </Link>
